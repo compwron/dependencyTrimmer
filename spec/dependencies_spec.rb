84b3aa1dbf49cb2ee46072eabd76bd225d6a0bbb
Linda Goldstein <lgoldste@thoughtworks.com>
2014-01-09 15:31:39 -0800

    one test for dep detection

--- spec/dependencies_spec.rb
+++ spec/dependencies_spec.rb
@@ -1,9 +1,10 @@
 require_relative "spec_helper"
 
 describe Dependencies do
-
-  describe "#card_data" do
-    it "" do
+  dependencies = Dependencies.new([])
+  describe "#get_dependencies" do
+    it "should not detect dependency in empty file text" do
+      dependencies.get_dependencies("").should be_empty
     end
   end
 end
